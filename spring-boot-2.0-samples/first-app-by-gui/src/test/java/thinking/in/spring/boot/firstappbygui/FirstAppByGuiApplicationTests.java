7d06ad7972b5d50a81f4af3c2c4db28a65d5c057
huhu <huhu_456321@163.com>
2019-11-27 22:27:53 +0800

    commit maven 版本和test类

--- spring-boot-2.0-samples/first-app-by-gui/src/test/java/thinking/in/spring/boot/firstappbygui/FirstAppByGuiApplicationTests.java
+++ spring-boot-2.0-samples/first-app-by-gui/src/test/java/thinking/in/spring/boot/firstappbygui/FirstAppByGuiApplicationTests.java
@@ -6,7 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//解决 mvn 打包问题
+@SpringBootTest(classes = FirstAppByGuiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FirstAppByGuiApplicationTests {
 
 	@Test
