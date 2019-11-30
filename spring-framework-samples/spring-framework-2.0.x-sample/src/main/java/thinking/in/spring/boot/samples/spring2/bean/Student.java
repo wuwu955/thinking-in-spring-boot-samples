df72048ef1b50bffcd66b7b7a3c6ad66e8a9911d
huhu <huhu_456321@163.com>
2019-11-30 15:30:36 +0800

    commit Spring 容器启动 加载不同环境配置

---
+++ spring-framework-samples/spring-framework-2.0.x-sample/src/main/java/thinking/in/spring/boot/samples/spring2/bean/Student.java
@@ -0,0 +1,19 @@
+package thinking.in.spring.boot.samples.spring2.bean;
+
+import lombok.Data;
+
+/**
+ * @author zhou.wu
+ * @version v1.0
+ * @package thinking.in.spring.boot.samples.spring2.bean
+ * @description: 学生对象
+ * @date 2019/11/30 3:01 PM
+ */
+@Data
+public class Student {
+
+   private String name;
+
+   private int age;
+
+}
--- spring-framework-samples/spring-framework-2.0.x-sample/src/main/java/thinking/in/spring/boot/samples/spring2/bootstrap/ConfigurableApplicationContextBootstrap.java
+++ spring-framework-samples/spring-framework-2.0.x-sample/src/main/java/thinking/in/spring/boot/samples/spring2/bootstrap/ConfigurableApplicationContextBootstrap.java
@@ -1,23 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package thinking.in.spring.boot.samples.spring2.bootstrap;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import thinking.in.spring.boot.samples.spring2.bean.Student;
 
 /**
  * 可配置的 Spring {@link ApplicationContext} 引导类
@@ -29,20 +14,33 @@ public class ConfigurableApplicationContextBootstrap {
 
     static {
         // 调整系统属性 "env"，实现 "name" bean 的定义切换
-        // envValue 可能来自于 "-D" 命令行启动参数
+        // envValue 可能来自于 "-Denv=dev" 命令行启动参数 java -jar -Denv=prod xx.jar
         // 参数当不存在时，使用 "prod" 作为默认值
-        String envValue = System.getProperty("env", "prod");
+        String envValue = System.getProperty("env");
+        if (envValue == null || "".equals(envValue)) {
+            envValue = "dev";
+        }
         System.setProperty("env", envValue);
     }
 
     public static void main(String[] args) {
-        // 定义 XML ApplicationContext
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/configurable-context.xml");
-        // "name" bean 对象
-        String value = (String) context.getBean("name");
-        // "name" bean 内容输出
-        System.out.println("Bean 'name' 的内容为：" + value);
-        // 关闭上下文
-        context.close();
+
+        try { // 定义 XML ApplicationContext
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("configurable-context.xml");
+            // "student" bean 对象
+            Student value = (Student) context.getBean("student");
+            // "name" bean 内容输出
+            System.out.println("Bean 'student' 的内容为：" + value);
+            // 启动
+            context.start();
+            //保持启动
+            while (context.isActive()) {
+                Thread.sleep(20);
+            }
+            System.exit(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("退出");
+        }
     }
 }
